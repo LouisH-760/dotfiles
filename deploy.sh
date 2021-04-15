9de076edcd37f6b12e8268b8d8f60adcdd046198
Louis Hermier <louis.hermier1@etu.univ-lorraine.fr>
2021-04-15 17:17:42 +0200

    Use relative paths, add wallpaper script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# fat chance this thing needs root
-
 LOCROFI="/usr/share/rofi/themes/"
 LOCCINNAMON="~/.themes/"
 
 echo "copying cinnamon theme"
-cp -r cinnamon/custom/ $LOCCINNAMON
+cp -r ./cinnamon/custom/ $LOCCINNAMON
 
 echo "copying rofi theme"
-sudo cp rofi/custom.rasi $LOCROFI
+sudo cp ./rofi/custom.rasi $LOCROFI
+
+echo "copying wallpaper script"
+cp ./wallpaper/wallpaper.sh ~
